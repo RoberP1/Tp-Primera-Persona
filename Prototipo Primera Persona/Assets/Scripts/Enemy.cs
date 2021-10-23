46e8819b25d06675a382c6c661c7b83805c79160
RoberP1 <roberpapeschi@gmail.com>
2021-10-23 19:35:13 -0300

    Agregado UI

--- Prototipo Primera Persona/Assets/Scripts/Enemy.cs
+++ Prototipo Primera Persona/Assets/Scripts/Enemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
@@ -13,9 +14,12 @@ public class Enemy : MonoBehaviour
     private Rigidbody rb;
     private bool alive;
     [SerializeField] private GameObject yo;
+    [SerializeField] private Slider Vida;
+
     void Start()
     {
         alive = true;
+        Vida.value = health;
         rb = GetComponent<Rigidbody>();
         agent = GetComponent<NavMeshAgent>();
         target = FindObjectOfType<Movimiento>().transform;
@@ -37,6 +41,7 @@ public class Enemy : MonoBehaviour
     public void Damage(float damage)
     {
         health -= damage;
+        Vida.value = health;
         if (alive && health <= 0)
         {
             agent.enabled = rb.isKinematic = alive = false;
