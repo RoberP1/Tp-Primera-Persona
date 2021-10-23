9d8039ede9e4914f02a86adb8b575e4ad9eca888
RoberP1 <roberpapeschi@gmail.com>
2021-10-23 03:46:07 -0300

    Agregado rotacion de camara

--- Prototipo Primera Persona/Assets/Scripts/Movimiento.cs
+++ Prototipo Primera Persona/Assets/Scripts/Movimiento.cs
@@ -8,17 +8,25 @@ public class Movimiento : MonoBehaviour
 
     Rigidbody rb = new Rigidbody();
     public float vel = 1;
+    public float sensivilidad = 1;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(new Vector3(0, Input.GetAxis("Mouse X") * sensivilidad, 0)));
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         rb.AddRelativeForce(new Vector3(horizontalInput, 0, verticalInput) * vel);
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
     }
 }
